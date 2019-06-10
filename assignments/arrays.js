1c8e61439171c82d4ae50ff93e7f1a671c69af57
Avni Patel Thompson <avnipatelthompson@Avnis-iMac.local>
2019-06-10 16:09:45 -0700

    checking in rest of assignment plus stretch goals

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -63,16 +63,13 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-let car33 = inventory.filter((car) => car.id === 33)[0];
-console.log(`Car 33 is a ${car33.car_year} ${car33.car_make} ${car33.car_model}` );
-
-
+console.log(`Car 33 is a ${inventory[32].car_year} ${inventory[32].car_make} ${inventory[32].car_model}`)
 
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = inventory[inventory.length - 1];
-console.log(`${lastCar.car_make} ${lastCar.car_model}`);
+console.log(lastCar.car_make, lastCar.car_model);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
@@ -80,8 +77,7 @@ let carModels = [];
 for (let i=0; i < inventory.length; i++) {
   carModels.push(inventory[i].car_model);
 }
-    carModels.sort();
-    console.log(carModels);
+    console.log(carModels.sort());
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
@@ -96,8 +92,11 @@ console.log(carYears);
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
 for (let i=0; i<carYears.length; i++) {
-  if (carYears[i]<2000)
+  if (carYears[i]<2000) {
     oldCars.push(carYears[i]);
+  }
+  else {
+  }
 }
 console.log(oldCars.length);
 
@@ -110,4 +109,4 @@ for (let i=0; i<inventory.length; i++) {
   BMWAndAudi.push(inventory[i]);
 }
 
-console.log(BMWAndAudi);
+console.log(JSON.stringify(BMWAndAudi));
